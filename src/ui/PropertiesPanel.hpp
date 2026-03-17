aeb0923e8dbc1ab6c193011e3a367366fc58f763
EgorMeshcheryakov1 <omoforiks@gmail.com>
2026-03-17 23:58:32 +0300

    feat: add selectedSegmentIndex to PropertiesPanel for polyline highlight

--- src/ui/PropertiesPanel.hpp
+++ src/ui/PropertiesPanel.hpp
@@ -9,9 +9,13 @@ class PropertiesPanel {
 public:
     bool render(core::Scene& scene, core::Viewport& viewport);
 
-    bool m_lockAnchor       = false;
-    bool m_lockProportions  = false;
+    bool m_lockAnchor             = false;
+    bool m_lockProportions        = false;
     bool m_drawOriginsOverFigures = false;
+
+    // Index of the polyline segment currently hovered/selected in the panel.
+    // -1 means none. main.cpp reads this to draw the highlight.
+    int  m_selectedSegmentIndex   = -1;
 };
 
 } // namespace ui
